ff9bad0a78e102a88b0508ba2c3f607e73bc0e81
Fabian Schmid <fabian.schmid@duke.edu>
2022-12-08 19:09:14 +0100

    Clean repo part2

--- 10_code/20_review+merge.py
+++ 10_code/20_review+merge.py
@@ -1,28 +1,23 @@
-# %%
 import pandas as pd
 import numpy as np
 import difflib
 from difflib import SequenceMatcher
+import os
+
+# Set working directory as location of current file to use relative paths further on
+dir = os.path.dirname(os.path.abspath(__file__))
 
 
-# %% [markdown]
-# ## merge ship data and population data
-
-# %%
 ##########################################################
-######### ADD FIPS CODE TO OPIOID DATA ###################
+######### merge ship data and population data ############
 ##########################################################
 
 
-import numpy as np
-import pandas as pd
+# Add FIPS code to opioid data
+opioid_data = pd.read_parquet(
+    os.path.join(dir, "../20_intermediate_files/opioid_data_woFIPS.parquet")
+)
 
-# Import aggregated drug data set
-# opioid_data = pd.read_csv(
-#    "C:/Users/fabi3/Documents/PythonExercises/opioid_project/drug_data_woFIPS.csv"
-# )
-
-opioid_data = pd.read_parquet("/Users/a563186832/Documents/GitHub/pds-2022-white-team/20_intermediate_files/opioid_data_woFIPS.parquet", engine="fastparquet")
 
 # Import FIPS data from US census
 url_fips = "https://www2.census.gov/geo/docs/reference/codes/files/national_county.txt"
@@ -111,79 +106,82 @@ merge["_merge"].value_counts()
 assert merge["_merge"].value_counts()[0] == len(opioid_data)
 
 
-# %% [markdown]
 # since the assert didn't break out, hence, we can conclude that our data is perfectly merged
 
-# %%
 # then we check whether our data covered all years
 merge.year.unique()
 
-# %% [markdown]
+
 # From the output, we covered all needed data to analyze the Washington Policy
 
-# %%
 merge = merge.drop(columns=["CountyName", "StateName", "_merge"])
 merge.head()
 
-# %% [markdown]
-# ## merge shipment data with fips
+## merge shipment data with population data
+df_pop = pd.read_parquet(
+    os.path.join(dir, "../20_intermediate_files/county_population.parquet")
+)
+df_ship_pop = pd.merge(
+    merge,
+    df_pop,
+    how="left",
+    left_on=["FIPS", "year"],
+    right_on=["FIPS", "year"],
+    validate="m:1",
+    indicator=True,
+)
 
-# %%
-# 
-df_pop = pd.read_parquet("/Users/a563186832/Documents/GitHub/pds-2022-white-team/20_intermediate_files/county_population.parquet", engine="fastparquet")
-df_ship_pop = pd.merge(merge,df_pop,how="left",left_on=["FIPS","year"],right_on=["FIPS","year"],validate="m:1",indicator=True)
+# Check whether some state has no population data
+df_ship_pop[df_ship_pop["_merge"] == "left_only"].BUYER_STATE.unique()
 
-# %%
-# Check whether some state has no FIPS code
-df_ship_pop[df_ship_pop["_merge"]=="left_only"].BUYER_STATE.unique()
 
-# %% [markdown]
-# From the output, only PR is missing with FIPS. PR is Puerto Rico which is a  Caribbean island and unincorporated U.S. territory and is not influencial in our analysis. So, we can safely drop those missing observations
+# From the output, only PR is missing with population data. PR is Puerto Rico which is a  Caribbean island and unincorporated U.S. territory and is not influencial in our analysis. So, we can safely drop those missing observations
 
-# %%
 df_ship_pop = df_ship_pop.drop(columns=["_merge"])
-df_ship_pop.to_csv("./ship_pop.csv",index=False)
-df_ship_pop.to_parquet("./ship_pop.parquet",engine="fastparquet")
+df_ship_pop.to_parquet(
+    os.path.join(dir, "../20_intermediate_files/ship_pop.parquet"), engine="fastparquet"
+)
 
-# %% [markdown]
-# ## merge overdose death data with population data
+##########################################################
+######### merge death data and population data ###########
+##########################################################
 
-# %%
 # load death data
-df_death= pd.read_parquet("/Users/a563186832/Documents/GitHub/pds-2022-white-team/20_intermediate_files/drug_overdosis_death.parquet", engine="fastparquet")
+df_death = pd.read_parquet(
+    os.path.join(dir, "../20_intermediate_files/drug_overdosis_death.parquet"),
+    engine="fastparquet",
+)
 df_death.head()
 
-# %%
 # merge with fips data
 
-df_death_pop = pd.merge(df_death,df_pop,how="left",left_on=["County Code","Year"],right_on=["FIPS","year"],validate="m:1",indicator=True)
+df_death_pop = pd.merge(
+    df_death,
+    df_pop,
+    how="left",
+    left_on=["County Code", "Year"],
+    right_on=["FIPS", "year"],
+    validate="m:1",
+    indicator=True,
+)
 
-# %%
-df_death_pop[df_death_pop["_merge"]=="left_only"].County.unique()
+df_death_pop[df_death_pop["_merge"] == "left_only"].County.unique()
 
-# %% [markdown]
 # From the output, we can see the counties which have no population data come from AK and VA. According to the instruction, we can safely ignore the Alaska area. Then we wento deal with the counties from VA
 
-# %% [markdown]
 # From Google, we found the FIPS for Bedford city, VA is 51515 and Clifton Forge city, VA is 51560
 
-# %%
-df_pop[(df_pop.FIPS==51515) | (df_pop.FIPS==51560)]
+df_pop[(df_pop.FIPS == 51515) | (df_pop.FIPS == 51560)]
 
-# %% [markdown]
 # We found that the FIPS 51515 and 51560 are missing. Since we don't include the VA in our analysis (it is not a target nor in control group). Hence, we may safely drop it.
 
-# %%
+
 # drop left only
-df_death_pop = df_death_pop.loc[df_death_pop._merge=="both",:]
+df_death_pop = df_death_pop.loc[df_death_pop._merge == "both", :]
 # drop merge column
 df_death_pop = df_death_pop.drop(columns=["_merge"])
 # write into parquet
-df_death_pop.to_parquet("./death_pop.parquet",engine="fastparquet")
-# write into csv
-df_death_pop.to_csv("./death_pop.csv",index=False)
-
-# %%
-
-
-
+df_death_pop.to_parquet(
+    os.path.join(dir, "../20_intermediate_files/death_pop.parquet"),
+    engine="fastparquet",
+)
---
+++ 10_code/31_shipment_analysis.py
@@ -0,0 +1,188 @@
+import pandas as pd
+import numpy as np
+import os
+from plotnine import *
+from warnings import filterwarnings
+
+ignore = filterwarnings("ignore")
+
+# Set working directory as location of current file to use relative paths further on
+dir = os.path.dirname(os.path.abspath(__file__))
+
+### Pre-Post Analysis
+
+## Florida
+
+# Read in the data
+df = pd.read_parquet(os.path.join(dir, "../20_intermediate_files/ship_pop.parquet"))
+df.head()
+
+# calculate the opioids per capita
+df["Opioid per capita"] = df["opioid_converted_grams"] / df["population"]
+df.head()
+
+# add binary of whether the state is before or after 2010 (policy change)
+df_fl = df[df["BUYER_STATE"] == "FL"]
+df_fl.loc[:, "Policy Change"] = df_fl["year"] > 2010
+df_fl.loc[:, "Years from Policy Change"] = df_fl["year"] - 2010
+
+treated_success = df_fl[df_fl["Policy Change"]]
+g = (
+    ggplot(treated_success, aes(x="Years from Policy Change", y="Opioid per capita"))
+    + geom_smooth(
+        method="lm",
+        data=df_fl[df_fl["Years from Policy Change"] < 0],
+    )
+    + geom_smooth(
+        method="lm",
+        data=treated_success[treated_success["Years from Policy Change"] >= 0],
+    )
+    + geom_vline(xintercept=0, linetype="dashed")
+    + geom_text(x=-0.7, y=0.5, label="Policy Change", color="black")
+    + labs(title="Pre-Post Model Graph, Policy Intervention")
+)
+g.save(
+    os.path.join(
+        dir, "../30_results/General_Results/florida_opioid_shipment_prepost.png"
+    )
+)
+print(g)
+
+# From the output of the graph, we can see the slope of regression model of opioid convert amount per capita is positive before the policy became effective, but changed to negative after the date the policy became effect. This means that the convert amount per capita increased year by year before the policy change and started to decrease annually after the policy effective date. Therefore, we may conclude that The policy is effective in Florida according to pre-post analysis.
+
+## Washington
+
+df_wa = df[df["BUYER_STATE"] == "WA"]
+df_wa.head()
+
+df_wa.loc[:, "Policy Change"] = df_wa["year"] > 2012
+df_wa.loc[:, "Years from Policy Change"] = df_wa["year"] - 2012
+
+
+treated_success = df_wa[df_wa["Policy Change"]]
+g = (
+    ggplot(treated_success, aes(x="Years from Policy Change", y="Opioid per capita"))
+    + geom_smooth(
+        method="lm",
+        data=df_wa[df_wa["Years from Policy Change"] < 0],
+    )
+    + geom_smooth(
+        method="lm",
+        data=treated_success[treated_success["Years from Policy Change"] >= 0],
+    )
+    + geom_vline(xintercept=0, linetype="dashed")
+    + geom_text(x=-0.9, y=0.21, label="Policy Change", color="black")
+    + labs(title="Pre-Post Model Graph, Policy Intervention")
+)
+g.save(
+    os.path.join(
+        dir, "../30_results/General_Results/washington_opioid_shipment_prepost.png"
+    )
+)
+print(g)
+
+# From the output, we can find that the slope of opioid conveted amount is positive before the date of policy being effective and became negative after 2012 in which year the policy became effective. Even though the gradient indicates that the decreasing speed is not high, but the overall trend is totally different from the previous years. Hence, we conclude that the policy has positive influence on the opioid converted amount restriction.
+
+### Diff-in-Diff
+
+## Florida
+
+# Select the states that we want to use as control group
+florida_compare = df[df["BUYER_STATE"].isin(["TN", "WV", "NV"])]
+florida_compare["Policy Change"] = florida_compare["year"] > 2010
+florida_compare["Years from Policy Change"] = florida_compare["year"] - 2010
+florida_compare["Compare"] = "Control Group"
+df_fl["Compare"] = "Florida"
+
+# We selected TN, WV, and NV since those three states have similar trends before the policy change
+
+success_model = df_fl[
+    ["Policy Change", "Years from Policy Change", "Opioid per capita", "Compare"]
+].copy()
+
+g = (
+    ggplot(
+        success_model,
+        aes(x="Years from Policy Change", y="Opioid per capita", color="Compare"),
+    )
+    + geom_smooth(
+        method="lm", data=success_model[success_model["Years from Policy Change"] < 0]
+    )
+    + geom_smooth(
+        method="lm", data=success_model[success_model["Years from Policy Change"] >= 0]
+    )
+    + geom_smooth(
+        method="lm",
+        data=florida_compare[florida_compare["Years from Policy Change"] < 0],
+    )
+    + geom_smooth(
+        method="lm",
+        data=florida_compare[florida_compare["Years from Policy Change"] >= 0],
+    )
+    + geom_vline(xintercept=0, linetype="dashed")
+    + geom_text(x=-0.7, y=7, label="Policy Change", color="black")
+    + labs(
+        title="Diff-in-Diff Model Graph, Policy Intervention",
+        color="Counties in State with Policy Change",
+    )
+    + theme(legend_position="right")
+)
+
+print(g)
+
+g.save(
+    os.path.join(
+        dir, "../30_results/General_Results/florida_opioid_shipment_diffdiff.png"
+    )
+)
+
+# From the output, we can see that the slope of control groups is still positive but the slope of Florida opioid convert amount annual increase decreased to negative. Therefore, we may conclude that the decrease of the opioid per caipta in Florida after 2010 is because of the policy which means the policy is effective.
+
+
+## Washington
+
+# Select the states that we want to use as control group
+wa_compare = df[df["BUYER_STATE"].isin(["IL", "WI", "RI"])]
+wa_compare["Policy Change"] = wa_compare["year"] > 2012
+wa_compare["Years from Policy Change"] = wa_compare["year"] - 2012
+wa_compare["Compare"] = "Control Group"
+df_wa["Compare"] = "Washington"
+
+success_model = df_wa[
+    ["Policy Change", "Years from Policy Change", "Opioid per capita", "Compare"]
+].copy()
+
+g = (
+    ggplot(
+        success_model,
+        aes(x="Years from Policy Change", y="Opioid per capita", color="Compare"),
+    )
+    + geom_smooth(
+        method="lm", data=success_model[success_model["Years from Policy Change"] < 0]
+    )
+    + geom_smooth(
+        method="lm", data=success_model[success_model["Years from Policy Change"] >= 0]
+    )
+    + geom_smooth(
+        method="lm", data=wa_compare[wa_compare["Years from Policy Change"] < 0]
+    )
+    + geom_smooth(
+        method="lm", data=wa_compare[wa_compare["Years from Policy Change"] >= 0]
+    )
+    + geom_vline(xintercept=0, linetype="dashed")
+    + geom_text(x=-0.5, y=0.3, label="Policy Change", color="black")
+    + labs(
+        title="Diff-in-Diff Model Graph, Effective Policy Intervention",
+        color="Counties in State with Policy Change",
+    )
+    + theme(legend_position="right")
+)
+
+g.save(
+    os.path.join(
+        dir, "../30_results/General_Results/washington_opioid_shipment_diffdiff.png"
+    )
+)
+print(g)
+
+# From the output, we can see the gradient change is similar for Washington and neiboughood states. Hence, we cannot conclude the policy is the only factor which deceased the opioid convert amount per capita.
